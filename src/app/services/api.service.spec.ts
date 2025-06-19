5eb4a8fa59790780bfc59e3f75245916f1e8cfc2
Faisal <pros3sa@gmail.com>
2025-06-19 11:27:11 +0000

    style: sanitize styles to use partials

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,33 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { ApiService} from './api.service';
-import { Comment } from '../models/comment';
-import { Post } from '../models/post';
+
+import { ApiService } from './api.service';
 
 describe('ApiService', () => {
   let service: ApiService;
-  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [ApiService]
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(ApiService);
-    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  afterEach(() => httpMock.verify());
-
-  it('should fetch posts', () => {
-    const mockPosts: Post[] = [{ id: 1, userId: 1, title: 'Test', body: 'Body' }];
-    service.getPosts().subscribe(posts => {
-      expect(posts.length).toBe(1);
-      expect(posts[0].title).toBe('Test');
-    });
-
-    const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/posts');
-    expect(req.request.method).toBe('GET');
-    req.flush(mockPosts);
+  it('should be created', () => {
+    expect(service).toBeTruthy();
   });
 });
