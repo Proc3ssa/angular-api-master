91aebbbdcae1097f3d52d6d566d92d5bc0bc9e00
Faisal <pros3sa@gmail.com>
2025-06-19 09:31:10 +0000

    test: add test for api.service

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ApiService } from './api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ApiService} from './api.service';
+import { Comment } from '../models/comment';
+import { Post } from '../models/post';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService]
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => httpMock.verify());
+
+  it('should fetch posts', () => {
+    const mockPosts: Post[] = [{ id: 1, userId: 1, title: 'Test', body: 'Body' }];
+    service.getPosts().subscribe(posts => {
+      expect(posts.length).toBe(1);
+      expect(posts[0].title).toBe('Test');
+    });
+
+    const req = httpMock.expectOne('https://jsonplaceholder.typicode.com/posts');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPosts);
   });
 });
